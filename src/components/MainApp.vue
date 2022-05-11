55d480436154e619c47d4d39fe8c3d3a3d6519c6
Stino01 <agostino.carrieri01@gmail.com>
2022-05-11 15:35:20 +0200

    milestone 2

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -5,11 +5,29 @@
             <button @click="getFilm">Cerca</button>
         </div>
         <div class="container">
-            <div class="info" v-for="(data, index) in filmList" :key="index">
-                <p>Titolo: {{data.title}}</p>
-                <p>Titolo Originale: {{data.original_title}}</p>
-                <p>Lingua: {{data.original_language}}</p>
-                <p>Voto: {{data.vote_average}}</p>
+            <div class="info" v-for="(data, index) in filmSeries" :key="index">
+                <ul>
+                    <li>
+                        <strong>Titolo:</strong>
+                        {{ data.title == null ? data.name : data.title }}
+                    </li>
+                    <li>
+                        <strong>Titolo Originale:</strong>
+                        {{ data.original_title == null ? data.original_name : data.original_title }}
+                    </li>
+                    <li v-if="!flag.includes(data.original_language)">
+                        <strong>Lingua:</strong>
+                        {{data.original_language}}
+                    </li>
+                    <li class="language" v-else>
+                        <strong>Lingua:</strong>
+                        <img class="flag" :src="require(`@/assets/img/${data.original_language}.png`)" :alt="data.original_language">
+                    </li>
+                    <li>
+                        <strong>Voto:</strong>
+                        {{data.vote_average}}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -23,20 +41,30 @@ export default {
   data() {
       return {
           inputText: "",
-          apiUrl: "https://api.themoviedb.org/3/search/movie",
+          apiUrl: "https://api.themoviedb.org/3/search/",
           apiKey: "api_key=f09bcfe2f55857ee3aca9a399323b3ff",
           apiLenguage: "lenguage=it-IT",
           apiQuery: "",
           filmList: [],
+          seriesList: [],
+          filmSeries: [],
+          flag: ['it', 'en', 'es', 'fr'],
       }
   },
   methods: {
+      getMovieApi() {
+          return axios.get(this.apiUrl + "movie?" + this.apiKey + "&query=" + this.apiQuery + "&" + this.apiLenguage);
+      },
+      getSeriesApi() {
+          return axios.get(this.apiUrl + "tv?" + this.apiKey + "&query=" + this.apiQuery + "&" + this.apiLenguage);
+      },
       getFilm(){
           this.apiQuery = this.inputText.replaceAll(" ", "+")
           //console.log(this.apiQuery)
-          axios.get(this.apiUrl + "?" + this.apiKey + "&query=" + this.apiQuery + "&" + this.apiLenguage).then((res) => {
-              this.filmList = res.data.results
-              //console.log(this.filmList)
+          Promise.all([this.getMovieApi(), this.getSeriesApi()]).then((res) => {
+              this.filmList = res[0].data.results
+              this.seriesList = res[1].data.results
+              this.filmSeries = this.filmList.concat(this.seriesList)
           }).catch((error) => {
             console.log(error)
           })
@@ -73,6 +101,18 @@ export default {
             padding: 1em;
             border: 1px solid black;
             border-radius: 10px;
+
+            ul {
+                list-style-type: none;
+
+                .language {
+                    display: flex;
+
+                    .flag {
+                        width: 1.5em;
+                    }
+                }
+            }
         }
     }
 </style>
